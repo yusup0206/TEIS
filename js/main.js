b8e3a58cfab0625c95768997ccca9560c89af551
yusup0206 <yusuph0206@gmail.com>
2023-04-01 11:29:49 +0500

    footer modal fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // SLIDER
 
-const swiper = new Swiper('.mySwiper', {
+const swiper = new Swiper(".mySwiper", {
   loop: true,
   slidesPerView: 1,
   spaceBetween: 0,
@@ -11,16 +11,16 @@ const swiper = new Swiper('.mySwiper', {
   },
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
 });
 
-const swiper2 = new Swiper('.mySwiper2', {
+const swiper2 = new Swiper(".mySwiper2", {
   loop: true,
   slidesPerView: 4,
   spaceBetween: 40,
@@ -31,11 +31,11 @@ const swiper2 = new Swiper('.mySwiper2', {
   },
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
   breakpoints: {
@@ -52,42 +52,42 @@ const swiper2 = new Swiper('.mySwiper2', {
 });
 
 // active nav
-const navEl = document.querySelector('.nav');
+const navEl = document.querySelector(".nav");
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (window.scrollY >= 178) {
-    navEl.classList.add('nav-scrolled');
+    navEl.classList.add("nav-scrolled");
   } else if (window.scrollY < 178) {
-    navEl.classList.remove('nav-scrolled');
+    navEl.classList.remove("nav-scrolled");
   }
 });
 
 // Burger
-const toggleBtn = document.querySelector('.toggle-btn');
-const navBurger = document.querySelector('.nav-burger');
+const toggleBtn = document.querySelector(".toggle-btn");
+const navBurger = document.querySelector(".nav-burger");
 
-toggleBtn.addEventListener('click', () => {
-  if (navBurger.classList.contains('active')) {
-    document.body.style.overflow = 'visible';
+toggleBtn.addEventListener("click", () => {
+  if (navBurger.classList.contains("active")) {
+    document.body.style.overflow = "visible";
   } else {
-    document.body.style.overflow = 'hidden';
+    document.body.style.overflow = "hidden";
   }
-  toggleBtn.classList.toggle('active');
-  navBurger.classList.toggle('active');
+  toggleBtn.classList.toggle("active");
+  navBurger.classList.toggle("active");
 });
 
-const burgerDropdownBtn = document.querySelector('.nav-burger-dropdown-btn');
-const burgerDropdown = document.querySelector('.nav-burger-dropdown');
+const burgerDropdownBtn = document.querySelector(".nav-burger-dropdown-btn");
+const burgerDropdown = document.querySelector(".nav-burger-dropdown");
 
-burgerDropdownBtn.addEventListener('click', () => {
-  burgerDropdownBtn.classList.toggle('active');
-  burgerDropdown.classList.toggle('active');
+burgerDropdownBtn.addEventListener("click", () => {
+  burgerDropdownBtn.classList.toggle("active");
+  burgerDropdown.classList.toggle("active");
 });
 
 // footer modal
-const footerBtn = document.querySelector('.footer-modal-btn');
-const footerModal = document.querySelector('.footer-modal');
+const footerBtn = document.querySelector(".footer-modal-btn");
+const footerModal = document.querySelector(".footer-modal");
 
-footerBtn.addEventListener('click', () => {
-  footerModal.classList.toggle('active');
+footerBtn.addEventListener("click", () => {
+  footerModal.classList.toggle("active");
 });
